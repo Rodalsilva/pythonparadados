21eecfb354fd632681be832f6423532fd064a6c7
Rodrigo Alves <r.alves2505@hotmail.com>
2022-06-07 21:32:02 -0300

    Começo do estudo do livro de Wes MacKinney

---
+++ Cap_2/hello_world.py
@@ -0,0 +1,5 @@
+print("Hello world")
+# É possível executar este programa com o comando 'python hello_world.py' no terminal.
+# No terminal, deve-se ter cuidado de abrir o arquivo no diretório em que ele está salvo.
+# No interpretador IPython, após chama-lo no terminal digitando ipython, o programa acima pode ser executado com o comando %run
+# %run hello_world.py
